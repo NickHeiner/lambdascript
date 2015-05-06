05d15a58f9dacd3a29c0d034fdd4a72be686ff95
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-06 17:16:29 -0400

    We have some simple tests running.
    They do not work yet, of course.

---
+++ lambdaconf-talk/MainTest.fs
@@ -0,0 +1,11 @@
+﻿module MainTest
+
+open NUnit.Framework
+
+[<Test>]
+let ``When 2 is added to 2 expect 4``() = 
+    Assert.AreEqual(4, 2+2)
+
+[<Test>]
+let ``When 2 is added to 2 expect 5``() = 
+    Assert.AreEqual(5, 2+2)
