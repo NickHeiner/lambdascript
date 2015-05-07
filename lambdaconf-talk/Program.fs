99d3f788a9547c192e33af8dc6a3fab983ab54d4
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-07 12:25:42 -0400

    Adding tests for lex

--- lambdaconf-talk/Program.fs
+++ lambdaconf-talk/Program.fs
@@ -126,19 +126,45 @@ open NUnit.Framework
 let ``tokenize - no input``() = 
     let actual = tokenize []
     let expected = []
-    Assert.AreEqual(actual, expected)
+    Assert.AreEqual(expected, actual)
     
 [<Test>]
 let ``tokenize - some input``() = 
     let actual = tokenize ["λ x -> 1"]
     let expected = ["λ"; "x"; "->"; "1"]
-    Assert.AreEqual(actual, expected)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``tokenize - many spaces``() = 
     let actual = tokenize ["λ     x  ->   1"]
     let expected = ["λ"; "x"; "->"; "1"]
-    Assert.AreEqual(actual, expected)
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``lex - no input``() = 
+    Assert.AreEqual(lex [], [])
+
+[<Test>]
+let ``lex - some input``() = 
+    let actual = lex ["λ"; "x"; "->"; "1"]
+    let expected = [
+        Lambda;
+        Identifier "x";
+        FuncArrow;
+        Literal "1"
+    ]
+    Assert.AreEqual(expected, actual)
+    
+[<Test>]
+let ``lex - different identifier``() = 
+    let actual = lex ["λ"; "f"; "->"; "1"]
+    let expected = [
+        Lambda;
+        Identifier "f";
+        FuncArrow;
+        Literal "1"
+    ]
+    Assert.AreEqual(expected, actual)
 (* end tests *)
 
 [<EntryPoint>]
