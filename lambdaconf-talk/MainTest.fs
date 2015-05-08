164f6487d66e7e04c9762be7cf7082a8c9480eb0
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-08 12:19:41 -0700

    Doing a bit of prototyping. Returning to a language you have not used in almost three years with no documentation is difficult, as it turns out. Ha.

--- lambdaconf-talk/MainTest.fs
+++ lambdaconf-talk/MainTest.fs
@@ -5,7 +5,3 @@ open NUnit.Framework
 [<Test>]
 let ``When 2 is added to 2 expect 4``() = 
     Assert.AreEqual(4, 2+2)
-
-[<Test>]
-let ``When 2 is added to 2 expect 5``() = 
-    Assert.AreEqual(5, 2+2)
