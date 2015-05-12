382971273f69f79b0fb127c57792d758b021b21f
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-12 16:25:53 -0700

    starting to work on cst to ast

---
+++ lambdaconf-talk/CstToAst.fs
@@ -0,0 +1,46 @@
+﻿module CstToAst
+
+open Grammar
+open Lex
+
+type Argument = 
+    | Lit of string
+    | Ident of string
+
+type FunctionDeclaration = {
+    functionName: string
+    argName: string
+    body: ParseTree
+    }
+
+type FunctionInvocation = {
+    func: ParseTree
+    arg: Argument
+    }
+
+type BooleanOperator = 
+    | Intersection
+    | Union
+    | Equal
+
+type BooleanExpression = {
+    leftHandSide: ParseTree
+    operator: BooleanOperator
+    rightHandSide: ParseTree
+    }
+
+type StringLookupInfo = {
+    string: ParseTree
+    regex: string
+    }
+
+type Ast =
+    | FunctionDecl of FunctionDeclaration
+    | FunctionInvoke of FunctionInvocation
+    | Boolean of BooleanExpression
+    | StringReLookup of StringLookupInfo
+
+let cstToAst (ast : ParseTree option) : Ast option = 
+    match ast with
+    | None -> None
+    | Some _ -> Some (FunctionInvoke { func = Leaf (Identifier "f"); arg = Ident "x"})
---
+++ lambdaconf-talk/CstToAstTest.fs
@@ -0,0 +1,31 @@
+﻿module CstToAstTest
+
+open NUnit.Framework
+open Lex
+open Grammar
+open CstToAst
+
+[<Test>]
+let ``cstToAst - string lookup`` () =
+    let expected = 
+        StringReLookup {
+            string = Leaf (Literal "foo")
+            regex = "^(f)?"
+        }
+
+    let actual = 
+        Expression [
+            StringLookup [
+                Expression [
+                    Leaf (Literal "foo")
+                ]
+                Leaf OpenSquareBracket
+                Leaf (RegexLiteral "^(f)?")
+                Leaf CloseSquareBracket
+            ]    
+        ]
+        |> Some
+        |> cstToAst
+
+    Assert.AreEqual(expected, actual)
+   
