807445958bf3bf05099e8ccaab7ac467ebecaad1
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-13 17:50:41 -0400

    Adding some support for converting to a JS AST. There are still rough edges.

---
+++ lambdaconf-talk/AstToJsAst.fs
@@ -0,0 +1,121 @@
+﻿module AstToJsAst
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+
+open CstToAst
+
+exception JsAstGenerationError of string
+
+let (astToJsAst : Ast option -> string option) = function
+    | None -> None
+    | Some ast -> 
+        let typeProp (value : string) = new JProperty("type", value)
+        
+        let inExpressionStatement (expr : JObject) =
+            new JObject([typeProp "ExpressionStatement"; new JProperty("expression", expr)])
+
+        let rec astToJsAstRec (ast : Ast) : JObject = 
+            let identObj (name : string) =
+                new JObject([typeProp "Identifier"; new JProperty("name", name)])
+
+            let callExpr (callee : JObject) (arguments : JObject) = 
+                new JObject([
+                                typeProp "CallExpression"
+                                new JProperty("callee", callee)
+                                new JProperty("arguments", new JArray([arguments]))
+                            ])
+
+            match ast with
+            | Lit value -> new JObject([typeProp "Literal"; new JProperty("value", value)])
+            | Ident name -> identObj name
+            | FunctionInvoke invocation -> 
+                let callee = astToJsAstRec invocation.func
+                let arguments = astToJsAstRec invocation.arg
+
+                callExpr callee arguments
+
+            | StringReLookup strLookup -> 
+                let memberExpression (obj : JObject) (property : JObject) (computed : bool) =
+                    new JObject([
+                                    typeProp "MemberExpression"
+                                    new JProperty("computed", computed)
+                                    new JProperty("object", obj)
+                                    new JProperty("property", property)
+                                ])
+
+                let regexMatch =
+                    let callee = 
+                        let propertyObj = new JObject([typeProp "Identifier"; new JProperty("name", "match")])
+                        let strToLookUp = astToJsAstRec strLookup.lookupSource
+                        memberExpression strToLookUp propertyObj false
+
+                    (* Not gonna lie this spacing is a bummer. *)
+                    let args = new JObject([
+                                                typeProp "Literal"
+                                                new JProperty("value", strLookup.regex)
+                                                new JProperty("regex", new JObject([
+                                                                                     new JProperty("pattern", sprintf "/%s/" strLookup.regex)
+                                                                                     new JProperty("flags", "")
+                                                                                     ]))
+                                            ])
+                
+                    callExpr callee args
+
+                let propertyObj = new JObject([typeProp "Literal"; new JProperty("value", 1)])
+                memberExpression regexMatch propertyObj true
+
+            | Bool boolExpr -> 
+                let jsOperator = match boolExpr.operator with
+                                 | Union -> "||"
+                                 | Intersection -> "&&"
+                                 | Equal -> "==="
+
+                new JObject([
+                                typeProp "LogicalExpression"
+                                new JProperty("operator", jsOperator)
+                                new JProperty("left", astToJsAstRec boolExpr.leftHandSide)
+                                new JProperty("right", astToJsAstRec boolExpr.rightHandSide)
+                            ])
+
+            | FunctionDecl funcDecl -> 
+                let idObj = identObj funcDecl.funcName
+                let paramsObj = new JArray([identObj funcDecl.argName])
+                let innerBody = astToJsAstRec funcDecl.body
+
+                let blockStatement = 
+                    new JObject([
+                                typeProp "BlockStatement"
+                                new JProperty("body", 
+                                    new JArray([
+                                               new JObject([typeProp "ReturnStatement"; new JProperty("argument", innerBody)])
+                                              ])
+                                    )
+                                ])
+
+                new JObject([
+                            typeProp "FunctionDeclaration"
+                            new JProperty("id", idObj)
+                            new JProperty("params", paramsObj)
+                            new JProperty("defaults", new JArray([]))
+                            new JProperty("body", blockStatement)
+                            new JProperty("generator", false)
+                            new JProperty("expression", false)
+                            ])
+
+            (* TODO: This is a failure to use the type system properly. *)
+            | Unknown -> JsAstGenerationError "An AST should not be of type Unknown" |> raise
+
+        let body = 
+            let innerJsAstWrapped = 
+                let innerJsAst = astToJsAstRec ast
+                if (innerJsAst.GetValue "type").ToString() = "FunctionDeclaration" 
+                then innerJsAst 
+                else inExpressionStatement innerJsAst
+
+            new JArray([innerJsAstWrapped])
+
+        new JObject([new JProperty("type", "Program"); new JProperty("body", body)])
+        |> JsonConvert.SerializeObject
+        |> Some
+
---
+++ lambdaconf-talk/AstToJsAstTest.fs
@@ -0,0 +1,326 @@
+﻿module AstToJsAstTest
+
+open NUnit.Framework
+open CstToAst
+open AstToJsAst
+open Newtonsoft.Json
+
+let areJsonEquivalent expected actual =
+    Assert.AreEqual(JsonConvert.DeserializeObject expected, JsonConvert.DeserializeObject actual)
+
+[<Test>]
+let ``astToJsAst - literal`` () =
+    let expected = """
+        {
+            "type": "Program",
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "expression": {
+                        "type": "Literal",
+                        "value": "hi"
+                    }
+                }
+            ]
+        }
+    """
+    
+    let actual = 
+        Lit "hi"
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - identifier`` () =
+    let expected = """
+        {
+            "type": "Program",
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "expression": {
+                        "type": "Identifier",
+                        "value": "identifier"
+                    }
+                }
+            ]
+        }
+    """
+    
+    let actual = 
+        Ident "identifier"
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - function invocation with identifier`` () =
+    let expected = """
+        {
+        "type": "Program",
+        "body": [
+            {
+                "type": "ExpressionStatement",
+                "expression": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "f"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Identifier",
+                            "name": "x"
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    """
+
+    let actual = 
+        FunctionInvoke {
+            func = Ident "f"
+            arg = Ident "x"
+        }
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - function invocation with argument as expression`` () =
+    let expected = """
+        {
+        "type": "Program",
+        "body": [
+            {
+                "type": "ExpressionStatement",
+                "expression": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "outer"
+                    },
+                    "arguments": [
+                        {
+                            "type": "CallExpression",
+                            "callee": {
+                                "type": "Identifier",
+                                "name": "innter"
+                            },
+                            "arguments": [
+                                {
+                                    "type": "Identifier",
+                                    "name": "argName"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    """
+
+    let actual = 
+        FunctionInvoke {
+            func = Ident "outer"
+            arg = FunctionInvoke {
+                func = Ident "inner"
+                arg = Ident "argName"
+            }
+        }
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - string lookup`` () =
+    let expected = """
+        {
+            "type": "Program",
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "expression": {
+                        "type": "MemberExpression",
+                        "computed": true,
+                        "object": {
+                            "type": "CallExpression",
+                            "callee": {
+                                "type": "MemberExpression",
+                                "computed": false,
+                                "object": {
+                                    "type": "Identifier",
+                                    "name": "str"
+                                },
+                                "property": {
+                                    "type": "Identifier",
+                                    "name": "match"
+                                }
+                            },
+                            "arguments": [
+                                {
+                                    "type": "Literal",
+                                    "value": "/(fe?)/",
+                                    "regex": {
+                                        "pattern": "(fe?)",
+                                        "flags": ""
+                                    }
+                                }
+                            ]
+                        },
+                        "property": {
+                            "type": "Literal",
+                            "value": 1
+                        }
+                    }
+                }
+            ]
+        }
+    """
+
+    let actual = 
+        StringReLookup {
+            lookupSource = Ident "str"
+            regex = "(fe?)"
+        }
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - boolean or`` () =
+    let expected = """
+        {
+            "type": "Program",
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "expression": {
+                        "type": "LogicalExpression",
+                        "operator": "||",
+                        "left": {
+                            "type": "Identifier",
+                            "name": "foo"
+                        },
+                        "right": {
+                            "type": "Identifier",
+                            "name": "bar"
+                        }
+                    }
+                }
+            ]
+        }
+    """
+    
+    let actual = 
+        Bool {
+            leftHandSide = Ident "foo"
+            operator = Union
+            rightHandSide = Ident "bar"
+        }
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - boolean and`` () =
+    let expected = """
+        {
+            "type": "Program",
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "expression": {
+                        "type": "LogicalExpression",
+                        "operator": "&&",
+                        "left": {
+                            "type": "Identifier",
+                            "name": "foo"
+                        },
+                        "right": {
+                            "type": "Identifier",
+                            "name": "bar"
+                        }
+                    }
+                }
+            ]
+        }
+    """
+    
+    let actual = 
+        Bool {
+            leftHandSide = Ident "foo"
+            operator = Intersection
+            rightHandSide = Ident "bar"
+        }
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
+
+[<Test>]
+let ``astToJsAst - function declaration`` () =
+    let expected = """
+        {
+            "type": "Program",
+            "body": [
+                {
+                    "type": "FunctionDeclaration",
+                    "id": {
+                        "type": "Identifier",
+                        "name": "f"
+                    },
+                    "params": [
+                        {
+                            "type": "Identifier",
+                            "name": "x"
+                        }
+                    ],
+                    "defaults": [],
+                    "body": {
+                        "type": "BlockStatement",
+                        "body": [
+                            {
+                                "type": "ReturnStatement",
+                                "argument": {
+                                    "type": "Literal",
+                                    "value": "constant value"
+                                }
+                            }
+                        ]
+                    },
+                    "generator": false,
+                    "expression": false
+                }
+            ]
+        }
+    """
+    
+    let actual = 
+        FunctionDecl {
+            funcName = "f"
+            argName = "x"
+            body = Lit "constant value"
+        }
+        |> Some
+        |> astToJsAst
+        |> Option.get
+
+    areJsonEquivalent expected actual
