a11985a7a90934ae66fa31dec66b0ad1485a480c
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-13 18:23:33 -0400

    running js!

---
+++ lambdaconf-talk/JsAstToJs.fs
@@ -0,0 +1,14 @@
+﻿module JsAstToJs
+
+open EdgeJs
+
+let jsAstToJs _ = 
+    let edgeFunc = Edge.Func @"
+        return function(data, cb) {
+            return cb(null, 1 + 1);
+        };
+    "
+
+    let task = edgeFunc.Invoke "no data to pass in right now"
+    task |> Async.AwaitTask |> ignore
+    task.Result
---
+++ lambdaconf-talk/JsAstToJsTest.fs
@@ -0,0 +1,9 @@
+﻿module JsAstToJsTest
+
+open NUnit.Framework
+open JsAstToJs
+
+[<Test>]
+let ``jsAstToJs - simple`` () =
+    let actual = jsAstToJs ""
+    Assert.AreEqual(2, actual)
