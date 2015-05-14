174f098b1f0aaa2a8f407ae36a4e30ff9135f62f
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-14 10:40:32 -0400

    adding another test

--- lambdaconf-talk/JsAstToJsTest.fs
+++ lambdaconf-talk/JsAstToJsTest.fs
@@ -8,3 +8,32 @@ let ``jsAstToJs - literal`` () =
     let json = """{"type": "ExpressionStatement", "expression": {"type": "Literal", "value": "string literal"}}"""
     let actual = jsAstToJs json
     Assert.AreEqual("'string literal';", actual)
+
+[<Test>]
+let ``jsAstToJs - function call`` () =
+    let json = """
+        {
+        "type": "Program",
+        "body": [
+            {
+                "type": "ExpressionStatement",
+                "expression": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "f"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Identifier",
+                            "name": "x"
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    """
+
+    let actual = jsAstToJs json
+    Assert.AreEqual("f(x);", actual)
