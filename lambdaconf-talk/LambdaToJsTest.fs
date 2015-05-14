bb3b8c2b4dd0a6191c1b7b8054cce6588c09f712
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-14 10:54:15 -0400

    holy shit this actually works end to end at least a little bit

--- lambdaconf-talk/LambdaToJsTest.fs
+++ lambdaconf-talk/LambdaToJsTest.fs
@@ -1,2 +1,12 @@
 ﻿module LambdaToJsTest
 
+open NUnit.Framework
+open LambdaToJs
+
+[<Test>]
+let ``lambdaToJs - method call`` () =
+    let expected = "print(isPalindrome('racecar'));"
+    let actual = lambdaToJs ["""print < isPalindrome "racecar" > """] |> Option.get
+
+    Assert.AreEqual(expected, actual)
+
