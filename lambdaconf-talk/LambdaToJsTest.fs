b7b663017095ef0ba8b77461e1d589a54987fb99
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-14 13:48:35 -0400

    Adding some tests for nearly-end-to-end functionality.

---
+++ lambdaconf-talk/LambdaToJsTest.fs
@@ -0,0 +1,39 @@
+﻿module LambdaToJsTest
+
+open NUnit.Framework
+open LambdaToJs
+
+[<Test>]
+let ``lambdaToJs - function invocation`` () =
+    let expected = "print(isPalindrome('racecar'));"
+    let actual = lambdaToJs ["""print < isPalindrome "racecar" > """] |> Option.get
+
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``lambdaToJs - string lookup`` () =
+    let expected = "str.match('ab(.)d')[1];"
+    let actual = lambdaToJs ["str[/ab(.)d/]"] |> Option.get
+
+    Assert.AreEqual(expected, actual)
+    
+[<Test>]
+let ``lambdaToJs - nested bool`` () =
+    let expected = "foo || bar && baz;"
+    let actual = lambdaToJs ["foo or < bar and baz >"] |> Option.get
+
+    Assert.AreEqual(expected, actual)
+    
+[<Test>]
+let ``lambdaToJs - nested bool with or`` () =
+    let expected = "(foo || bar) && baz;"
+    let actual = lambdaToJs ["< foo or bar > and baz"] |> Option.get
+
+    Assert.AreEqual(expected, actual)
+    
+[<Test>]
+let ``lambdaToJs - function declaration`` () =
+    let expected = "function f(x) {\n    return 'retVal';\n}"
+    let actual = lambdaToJs ["""λ @f :x . "retVal" """] |> Option.get
+
+    Assert.AreEqual(expected, actual)
