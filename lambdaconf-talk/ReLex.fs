4d435fb99e78facd0c8a15a2ae27004905a40d4d
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-18 17:30:36 -0400

    making the tests pass

---
+++ lambdaconf-talk/ReLex.fs
@@ -0,0 +1,21 @@
+﻿module ReLex
+
+open Lex
+
+let reLex = 
+    List.fold (fun acc el -> 
+        let nextElem = 
+            match el with
+            | Identifier ident -> 
+                match acc with
+                | [] -> el
+                | hd::tl -> match hd with
+                            | Lambda -> FuncName ident
+                            | FuncName _ -> ArgName ident
+                            | _ -> el
+            | _ -> el
+
+        nextElem::acc
+    ) [] 
+    >> List.rev
+
