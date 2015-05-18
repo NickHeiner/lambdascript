734473785684ef1c6a0933d2e0ddace0bafdcd74
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-18 17:39:55 -0400

    Making the lexer a little more context-sensitive to make the language cleaner.

---
+++ lambdaconf-talk/ReLexTest.fs
@@ -0,0 +1,19 @@
+﻿module ReLexTest
+
+open Lex
+open ReLex
+open NUnit.Framework
+
+[<Test>]
+let ``reLex - identity`` () =
+    let expected = [OpenAngleBracket; Identifier "x"; CloseAngleBracket]
+    let actual = reLex expected
+
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``reLex - func declaration`` () =
+    let expected = [Lambda; FuncName "f"; ArgName "x"; FuncDot; Literal "literal value"]
+    let actual = reLex [Lambda; Identifier "f"; Identifier "x"; FuncDot; Literal "literal value"]
+
+    Assert.AreEqual(expected, actual)
