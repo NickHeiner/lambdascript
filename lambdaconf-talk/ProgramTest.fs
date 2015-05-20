26bf9ee499dfff50ea1eef4ba3dc725337d89c91
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-20 17:27:41 -0400

    adding an e2e test

---
+++ lambdaconf-talk/ProgramTest.fs
@@ -0,0 +1,15 @@
+﻿module ProgramTest
+
+open NUnit.Framework
+
+[<Test>]
+let ``program - run sample.lambda`` () =
+    let testProc = 
+        System.Diagnostics.Process.Start
+            ("lambdaconf_talk.exe", "..\..\sample.lambda")
+
+    testProc.WaitForExit ()
+
+    Assert.AreEqual(0, testProc.ExitCode)
+
+    testProc.Dispose()
