3f57260f13a6ee5895f2b782e029172972c4505f
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-20 17:30:55 -0400

    silencing logs

--- lambdaconf-talk/Util.fs
+++ lambdaconf-talk/Util.fs
@@ -1,4 +1,7 @@
 ﻿module Util
 
+(* I could have a more sophisticated way of doing this but w/e. *)
+let SILENCE_LOGS = true
+
 (* Is there a real log system in f#? *)
-let log label obj = printfn "%s %A" label obj
+let log label obj = if not SILENCE_LOGS then printfn "%s %A" label obj else ()
