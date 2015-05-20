72d0fb2ad846f3cdff85337a5adca51e3fb8d211
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-20 11:00:47 -0400

    adding failing test

--- lambdaconf-talk/LambdaToJsTest.fs
+++ lambdaconf-talk/LambdaToJsTest.fs
@@ -3,6 +3,10 @@
 open NUnit.Framework
 open LambdaToJs
 
+(* Instead of js string comparison, it would be nice to compare ASTs so we don't worry about formatting. 
+   Additionally, it would be nice to actually test the js that was output.
+*)
+
 [<Test>]
 let ``lambdaToJs - function invocation`` () =
     let expected = "print(isPalindrome('racecar'));"
@@ -35,5 +39,16 @@ let ``lambdaToJs - nested bool with or`` () =
 let ``lambdaToJs - function declaration`` () =
     let expected = "function f(x) {\n    return 'retVal';\n}"
     let actual = lambdaToJs ["""λ f x . "retVal" """] |> Option.get
+
+    Assert.AreEqual(expected, actual)
+
+(* TODO there is a bug where `print "hello world"` does not compile correctly. 
+   I suspect that the tokenizier is broken.
+*)
+
+[<Test>]
+let ``lambdaToJs - print statement`` () =
+    let expected = "var print = console.log.bind(console);\nprint('hello world');"
+    let actual = lambdaToJs ["""print "hello" """] |> Option.get
 
     Assert.AreEqual(expected, actual)
