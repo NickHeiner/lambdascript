d970f8e73fea2202457b1eb4ad87745898d68eb0
Nick Heiner <nick.heiner+github@gmail.com>
2015-05-21 20:24:15 -0600

    hacking this shit through

--- lambdaconf-talk/Program.fs
+++ lambdaconf-talk/Program.fs
@@ -3,21 +3,28 @@
 open ReadFile
 open Util
 open System.Diagnostics
+open System.IO
 
 open LambdaToJs
 
 [<EntryPoint>]
 let rec main argv = 
     let entryPoint = argv.[0]
+    let writeToFile = if argv.Length >= 2 then Some argv.[1] else None
     log "Entry point file: %s" entryPoint
     
     let result = 
         GetFileContents entryPoint 
         |> lambdaToJs
 
-
     match result with
     | Some js -> 
+        
+        match writeToFile with
+        | Some fileName ->
+            log "Writing output js to %s" fileName
+            File.WriteAllText(fileName, js, System.Text.Encoding.UTF8)
+        | None -> ()
 
         printfn """
 ---
@@ -30,16 +37,21 @@ Compilation complete:
 
         let startInfo = new ProcessStartInfo("node", sprintf "-e \"%s\"" js)
         startInfo.RedirectStandardOutput <- true
+        startInfo.RedirectStandardError <- true
         startInfo.UseShellExecute <- false
         startInfo.CreateNoWindow <- true
         let nodeProc = Process.Start(startInfo)
         nodeProc.WaitForExit()
 
         printfn """
-Program output:
+Program stdout:
 ---
 %s
-              """ (nodeProc.StandardOutput.ReadToEnd())
+
+Program stderr:
+---
+%s
+              """ (nodeProc.StandardOutput.ReadToEnd()) (nodeProc.StandardError.ReadToEnd())
               
         nodeProc.Dispose()
 
--- lambdaconf-talk/Util.fs
+++ lambdaconf-talk/Util.fs
@@ -3,7 +3,7 @@
 (* Is there a real log system in f#? *)
 
 (* I could have a more sophisticated way of doing this but w/e. *)
-let SILENCE_DEBUG_LOGS = false
+let SILENCE_DEBUG_LOGS = true
 
 let log label obj = if not SILENCE_DEBUG_LOGS then printfn "%s %A" label obj else ()
 
