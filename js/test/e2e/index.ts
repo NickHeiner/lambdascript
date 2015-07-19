d7c3329349c811c8e753f98977d1bd0fcffa27c6
Nick Heiner <nick.heiner@opower.com>
2015-07-19 19:18:52 -0400

    well at least it builds now but no test is run

--- js/test/e2e/index.ts
+++ js/test/e2e/index.ts
@@ -9,29 +9,33 @@ const test = require('tape'),
     child_process = require('child_process'),
     lsc = require('../..');
 
-test('lambdascript compiler in js', function(t) {
-   function runTest(testFileName: string) {
-       const testFilePath = path.resolve(__dirname, '..', 'fixtures', testFileName);
+test('lambdascript compiler in js', function(t: any) {
+    function runTest(testFileName: string) {
+        const testFilePath = path.resolve(__dirname, '..', 'fixtures', testFileName);
 
-       return q.nfcall(tmp.file.bind(tmp))
-           .then(function(jsOutputFilePath) {
+        return q.nfcall(tmp.file.bind(tmp))
+            .then(function(jsOutputFilePath: string) {
                return lsc(testFilePath, jsOutputFilePath)
                    .then(function() {
                        return q.nfcall(child_process.spawn.bind(child_process), 'node', jsOutputFilePath);
                    });
-           }).then(function(stdout, stderr) {
+            }).then(function(stdout: string, stderr: string) {
                if (stderr) {
                    throw new Error(stderr);
                }
 
                return stdout;
-           });
-   }
+            });
+    }
 
-    t.test('print boolean', function(t) {
+    function stringEqual(t: any, str1: string, str2: string, message: string): void {
+        t.equal(str1, str2, message);
+    }
+
+    t.test('print boolean', function(t: any) {
         t.plan(1);
-        runTest('print-boolean.lambda').then(function(stdout) {
-            t.equal(stdout, 'true', 'boolean is printed correctly to standard out');
+        runTest('print-boolean.lambda').then(function(stdout: string) {
+            stringEqual(t, stdout, 'true', 'boolean is printed correctly to standard out');
         });
     });
 });
