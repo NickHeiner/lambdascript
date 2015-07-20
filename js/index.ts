7dde106d78ed970d9c8f62401de5735bb774bbd4
Nick Heiner <nick.heiner@opower.com>
2015-07-20 16:26:07 -0400

    well at least now the error is because the jison file is busted

--- js/index.ts
+++ js/index.ts
@@ -2,11 +2,15 @@
 
 'use strict';
 
-const q = require('q');
+const q = require('q'),
+    lambda = require('./lambda'),
+    qFs = require('q-io/fs');
 
 function lsc(inputLambdaScriptFile: string, outputJsFile: string) {
-    console.log('# not yet implemented');
-    return q();
+    return qFs.read(inputLambdaScriptFile).then(function(lambdaScriptCode: string) {
+        const js = lambda.parser.parse(lambdaScriptCode);
+        return qFs.write(outputJsFile, js);
+    });
 }
 
 // Why is this necessary?
