d9ec2b6e776551de288937b7278e6d4e44077bad
Nick Heiner <nick.heiner@opower.com>
2015-08-05 14:46:52 -0400

    initial structure

--- js/lib/get-highlighted-code.ts
+++ js/lib/get-highlighted-code.ts
@@ -1,7 +1,32 @@
 import '../types';
 
+const chalk = require('chalk'),
+    _str = require('underscore.string'),
+    traverse = require('traverse'),
+    os = require('os'),
+
+    colorMap: IStringMap = {
+        Identifier: 'cyan'
+    };
+
+interface IStringMap {
+    [key: string]: string;
+}
+
 function getHighlightedCode(lscAst: ILambdaScriptAstNode, lambdaScriptCode: string) {
-    return lambdaScriptCode;
+    const codeByLines = _str.lines(lambdaScriptCode),
+        coloredCode = traverse(lscAst).reduce(function(acc: string, astNode: ILambdaScriptAstNode) {
+            const color = colorMap[astNode.type];
+
+            if (color) {
+                const colorFn = chalk[color];
+                console.log(colorFn('color'));
+            }
+
+            return acc;
+        }, codeByLines);
+
+    return coloredCode.join(os.EOL);
 }
 
 export = getHighlightedCode;
